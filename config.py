331c32aec65e0e9cd77d1829143efffb7e03e27a
tishetiho <moteoAZ@yandex.ru>
2026-03-23 14:30:50 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+TOKEN = "8698564106:AAG2Y7Yqicd5QDIKN7yy-fZ5ptOrMbkq0VU" # Токен бота
+CRYPTO = "555071:AAAYMNf6CF85JZL1ED4LudySN62Kfd2S2Iq" # Токен CryptoBot
+ADMINS = [5078764886] # ID Админов
+
+# Bot Options
+bot_logs = "" 
+bot_channel_link = ""
+bot_admin = ""
+
+bot_channel = ""
+bot_information = ""
+
+# Price
+subscribe_1_day = 1.5
+subscribe_7_days = 5
+
+subscribe_30_days = 8
+
+subscribe_infinity_days = 12
